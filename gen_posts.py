9b2fabd5459770bc7609eb904fb435cd7cf4707d
witness-s1 <witness-s1@protonmail.com>
2019-07-03 18:09:17 -0700

    Refactor

--- gen_posts.py
+++ gen_posts.py
@@ -14,11 +14,14 @@ def gen_posts():
     feed = feedparser.parse(url)
     time_now = datetime.now()
     for idx, entry in enumerate(feed["entries"]):
+        # Filter invalid characters.
         title = re.sub(r'[\/:"*?<>|]+', '', entry["title"]) 
+        # Jeyll force all posts should have time at beginning.
         filename = time_now.strftime(f"%Y-%m-%d-{title}.md")
+        # Shift blog post time to order them correctly.
         delta = timedelta(minutes=idx)
         with open(os.path.join("_posts", filename), "w") as out:
-            layout = f"""
+            header = f"""
 ---
 layout: post
 title: "{entry["title"]}"
@@ -26,7 +29,7 @@ date: {time_now - delta}
 ---
 
 """.lstrip()
-            out.write(layout)
+            out.write(header)
             out.write(html2text.html2text(entry["content"][0]["value"]))
 
 if __name__ == "__main__":
